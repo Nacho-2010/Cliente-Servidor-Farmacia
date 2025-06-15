090056ba3f86bc3b8ae94a036f70ab6638aa0c69
Josue Navarro <josue13398@icloud.com>
2025-06-15 14:07:58 -0600

    Actualización de principal.php, principal.css y nuevo principal.js

---
+++ Views/assets/Funciones/principal.js
@@ -0,0 +1,11 @@
+// ======= Sección: este js es para el button del login del home  =======
+function openModal() {
+  document.getElementById("modalOverlay").className = "overlay open";
+  document.getElementById("modalDialog").className = "dialog open";
+}
+
+function closeModal() {
+  document.getElementById("modalOverlay").className = "overlay closed";
+  document.getElementById("modalDialog").className = "dialog closed";
+}
+
