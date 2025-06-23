e3e17a6027a9e40bda1574db2fb1bd1c55fdcfab
Josue Navarro <josue13398@icloud.com>
2025-06-22 23:19:02 -0600

    necesario en models para conectar con bd

---
+++ Models/loginModel.php
@@ -0,0 +1,55 @@
+<?php
+
+include_once $_SERVER["DOCUMENT_ROOT"] . '/Cliente-Servidor-Farmacia/Models/connect.php';
+function ValidarInicioSesionModel($correo, $contrasena)
+{
+    try {
+        $conexion = OpenDB();
+
+        $sp = "CALL ValidarInicioSesion('$correo', '$contrasena')";
+        $respuesta = $conexion->query($sp);
+
+        CloseDB($conexion);
+        return $respuesta;
+    } catch (Exception $error) {
+        RegistrarError($error);
+        return null;
+    }
+}
+
+
+function RegistrarUsuarioModel($nombre, $correo, $usuario, $contrasena)
+{
+    try {
+        $conexion = OpenDB();
+
+        $sp = "CALL RegistrarUsuario('$nombre', '$correo', '$usuario', '$contrasena')";
+        $respuesta = $conexion->query($sp);
+
+        CloseDB($conexion);
+        return $respuesta;
+    } catch (Exception $error) {
+        RegistrarError($error);
+        return false;
+    }
+}
+
+
+
+function ValidarCorreoModel($correo)
+{
+    try {
+        $conexion = OpenDB();
+
+        $sp = "CALL ValidarCorreo('$correo')";
+        $respuesta = $conexion->query($sp);
+
+        CloseDB($conexion);
+        return $respuesta;
+    } catch (Exception $error) {
+        RegistrarError($error);
+        return null;
+    }
+}
+
+?>
