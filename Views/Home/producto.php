3421512fcc779e5833b6846b13d0cef2abacc8fe
Abraham3105 <151878813+Abraham3105@users.noreply.github.com>
2025-07-18 23:25:10 -0600

    Creacion de tabla

--- Views/Home/producto.php
+++ Views/Home/producto.php
@@ -1,37 +1,141 @@
 <?php
+// Incluye tu layout y controlador
 require_once $_SERVER["DOCUMENT_ROOT"] . '/Cliente-Servidor-Farmacia/Views/layout.php';
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-
-<?php
-añadirCSS(); //<head> con CSS
-?>
+<?php añadirCSS(); ?>
 
 <body>
 
-  <?php
-  verheader();
-  ?>
+<?php
+  verheader(); 
+  sidebar();   
+?>
 
-  <?php
-  sidebar();
-  ?>
+<main style="margin-left:13%; padding: 2rem;">
+  <div class="page-wrapper">
+    <div class="container-fluid">
 
-  <main style="margin-left: 13%">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Consulta de Productos</h4>
+            </div>
+            <hr>
+            <form class="form-horizontal" action="" method="POST">
+              <div class="card-body">
 
-  </main>
+                <?php
+                  if(isset($_POST["txtMensaje"])) {
+                      echo '<div class="alert alert-warning text-center">' . $_POST["txtMensaje"] . '</div>';
+                  }
+                ?>
 
-  <?php
+                <div class="row">
+                  <div class="col-md-12 text-right pb-4">
+                    <a href="registrarProducto.php" class="btn btn-info">Agregar</a>
+                  </div>
+                </div>
 
-  verfooter();
-  ?>
+                <table id="tablaDatos" class="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Nombre</th>
+                      <th>Descripción</th>
+                      <th>Precio</th>
+                      <th>Cantidad</th>
+                      <th>Imagen</th>
+                      <th>Estado</th>
+                      <th>Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                      while($fila = mysqli_fetch_array($resultado)) {
+                        echo "<tr>";
+                        echo "<td>". $fila["IdProducto"] ."</td>";
+                        echo "<td>". $fila["Nombre"] ."</td>";
+                        echo "<td>". $fila["Descripcion"] ."</td>";
+                        echo "<td>". $fila["Precio"] ."</td>";
+                        echo "<td>". $fila["Cantidad"] ."</td>";
+                        echo "<td><img src='". $fila["Imagen"] ."' width='125' height='125'></td>";
+                        echo "<td>". $fila["EstadoDescripcion"] ."</td>";
+                        echo '<td>
+                          <a class="btn btnAbrirModal" data-toggle="modal" data-target="#CambiarEstadoProducto"
+                            data-id="' . $fila["IdProducto"] . '" data-nombre="' . $fila["Nombre"] . '">
+                            <i class="fa ' . ($fila["Estado"] ? 'fa-toggle-on text-success' : 'fa-toggle-off text-danger') . '" style="font-size:1.5em;"></i>
+                          </a>
+                          <a class="btn">
+                            <i class="fa fa-edit" style="font-size:1.5em;"></i>
+                          </a>
+                        </td>';
+                        echo "</tr>";
+                      }
+                    ?>
+                  </tbody>
+                </table>
 
-  <?php
-  añadirScripts(); // Todos los scripts JS 
-  ?>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="CambiarEstadoProducto" tabindex="-1" role="dialog" aria-labelledby="tituloModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="tituloModal">Confirmación</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        
+        <form action="" method="POST">
+          <div class="modal-body">
+            <input type="hidden" id="IdProducto" name="IdProducto" class="form-control">
+            <label id="lblNombre" name="lblNombre"></label>
+          </div>
+          <div class="modal-footer">
+            <button type="submit" id="btnCambiarEstadoProducto" name="btnCambiarEstadoProducto" class="btn btn-primary">Procesar</button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php verfooter(); ?>
+<?php añadirScripts(); ?>
+
+<script>
+  $(document).ready(function() {
+    new DataTable('#tablaDatos', {
+      language: {
+        url: 'https://cdn.datatables.net/plug-ins/2.3.2/i18n/es-ES.json',
+      },
+    });
+
+    $('.btnAbrirModal').on('click', function () {
+      const id = $(this).data('id');
+      const nombre = $(this).data('nombre');
+      $('#IdProducto').val(id);
+      $('#lblNombre').text("¿Desea cambiar el estado del producto " + nombre + "?");
+    });
+  });
+</script>
 
 </body>
-
 </html>
